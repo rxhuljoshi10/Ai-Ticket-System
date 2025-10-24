c5796ade8cb3bb5141b40e188167f623d0412d01
Rahul Joshi <joshirahul7040@gmail.com>
2025-10-24 23:39:46 +0530

    Project file structure

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDb Connected!");
+        app.listen(PORT, () => console.log("Server at http://localhost:3000"))
+    })
+    .catch((err) => console.error("MongoDb error!", err))
